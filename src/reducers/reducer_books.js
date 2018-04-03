4a5074ec74065aa629a479db4fb87801c88f5852
Megan Walter <megan.elizabeth.walter@gmail.com>
2018-04-03 15:38:53 -0400

    finishing the simple redux book app example

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    {title: 'Javascript', pages: 200 },
+    {title: 'Harry Potter', pages: 80},
+    {title: 'The Dark Tower', pages: 300},
+    {title: 'Eloquent Ruby', pages: 1}
+  ]
+}
